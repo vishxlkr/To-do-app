151c76f5ef013c50833a9c038430407d0d930772
vishxlkr <vishxlkr@gmail.com>
2026-03-14 06:06:52 +0530

    update

--- internal/handlers/todo_handler.go
+++ internal/handlers/todo_handler.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"net/http"
+	"todo/internal/repository"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type CreateTodoInput struct {
+	Title     string `json:"title" binding:"required"`
+	Completed string `json:"completed"`
+}
+
+func CreateTodoHandler(pool *pgxpool) gin.HandlerFunc{
+
+	return func (c *gin.Context){
+		var input CreateTodoInput
+		
+		if err := c.ShouldBindJSON(&input); err!=nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error()
+			})
+		}
+		
+		// if there is no error then this is continue 
+
+		todo , err :=  repository.CreateTodo(pool )
+	}
+
+}
--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -8,10 +8,10 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func CreateTodo(pool *pgxpool.Pool, title string, complted bool) (*models.Todo, error) {
+func CreateTodo(pool *pgxpool.Pool, title string, completed bool) (*models.Todo, error) {
 	var ctx context.Context
 	var cancel context.CancelFunc
-	context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 
 	defer cancel()
 
@@ -22,9 +22,9 @@ func CreateTodo(pool *pgxpool.Pool, title string, complted bool) (*models.Todo, 
 
 	`
 
-	var todo model.Todo
+	var todo models.Todo
 
-	var err error = pool.QueryRow(ctx, query, title, complted).Scan(
+	var err error = pool.QueryRow(ctx, query, title, completed).Scan(
 		&todo.ID,
 		&todo.Title,
 		&todo.Completed,
