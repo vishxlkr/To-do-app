d40792878248c0e0cad48e88e2628fd9e69e8ee3
vishxlkr <vishxlkr@gmail.com>
2026-03-15 13:29:37 +0530

    update

--- internal/handlers/todo_handler.go
+++ internal/handlers/todo_handler.go
@@ -171,3 +171,40 @@ func UpdateToDoHandler(pool *pgxpool.Pool) gin.HandlerFunc {
 	}
 
 }
+
+func DeleteToDoHandler(pool *pgxpool.Pool) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		idStr := c.Param("id")
+
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid todo ID",
+			})
+			return
+		}
+
+		err = repository.DeleteToDo(pool, id)
+
+		if err != nil {
+			if err.Error() == "todoTodo with id "+idStr+" not found" {
+				c.JSON(http.StatusNotFound, gin.H{
+					"error": "Todo not found",
+				})
+				return
+			}
+
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Todo deleted successfully.",
+		})
+
+	}
+}
