f5dd190ad5d8dafad4bef89b07b5879cfb450cf8
vishxlkr <vishxlkr@gmail.com>
2026-03-15 13:48:31 +0530

    update

---
+++ internal/models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	ID        string `json:"id" db:"id"`
+	Email     string `json:"email" db:"email"`
+	Password  string `json:"password" db:"password"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
+}
