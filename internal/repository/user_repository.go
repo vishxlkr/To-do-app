e8dc83991639282173963563614299cba01b320d
vishxlkr <vishxlkr@gmail.com>
2026-03-15 14:06:11 +0530

    update

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -50,13 +50,40 @@ func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
 		SELECT id , email , password , created_at , updated_at
 		FROM users
 		WHERE EMAIL = $1
-		
-	
 	`
-
 	var user models.User
 
 	err := pool.QueryRow(ctx, query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+
+}
+
+func GetUserByID(pool *pgxpool.Pool, id int) (*models.User, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+		SELECT id , email , password , created_at , updated_at
+		FROM users
+		WHERE ID = $1
+	`
+	var user models.User
+
+	err := pool.QueryRow(ctx, query, id).Scan(
 		&user.ID,
 		&user.Email,
 		&user.Password,
