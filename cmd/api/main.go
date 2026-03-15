8e44886ef816a5fdcd690b31d527e77f2986ad5a
vishxlkr <vishxlkr@gmail.com>
2026-03-15 13:31:51 +0530

    update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -45,6 +45,7 @@ func main() {
 	router.GET("/todos", handlers.GetAllTodosHandler(pool))
 	router.GET("/todos/:id", handlers.GetToDoByIDHandler(pool))
 	router.PUT("/todos/:id", handlers.UpdateToDoHandler(pool)) // body will also be sent
+	router.DELETE("/todos/:id", handlers.DeleteToDoHandler(pool))
 
 	router.Run(":" + cfg.Port) // this start the server at given port
 }
